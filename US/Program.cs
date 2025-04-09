9be304711edc3c9c7792bf658f204acdab188038
unknown <yoshikuni335@gmail.com>
2025-04-09 18:03:04 +0700

    Chạy code entity để binding ra bảng CSDL

--- US/Program.cs
+++ US/Program.cs
@@ -16,7 +16,7 @@ namespace US
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new notification());
         }
     }
 }
